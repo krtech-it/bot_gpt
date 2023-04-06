b77e561d0d907a2f817bb9883758c9f09eba806f
Кирилл Петухов <test@mail.ru>
2023-04-06 22:35:03 +0300

    вывод списка комнат и создание комнаты

--- bot.py
+++ bot.py
@@ -9,26 +9,44 @@ if not DataBaseWork.check_db():
     DataBaseWork.start_db()
 
 
-user_id = 736460899
-user = DataBaseWork.get_user(user_id)
-if user is None:
-    user = DataBaseWork.create_user(user_id, "name")
+# user_id = 736460899
+# user = DataBaseWork.get_user(user_id)
+# if user is None:
+#     user = DataBaseWork.create_user(user_id, "name")
 
-rooms = DataBaseWork.get_all_rooms_user(user_id)
-print(rooms)
 
-# @bot.message_handler(commands=['start'])
-# def get_weather(message):
-#     user_id = message.chat.id
-#     user = DataBaseWork.get_user(user_id)
-#     if user is None:
-#         user = DataBaseWork.create_user(user_id, message.chat.first_name)
-#     bot.send_message(message.chat.id, text='Hello, {name}'.format(name=user.name))
+# DataBaseWork.create_room(user.id, 'test_room')
 #
-#
-# @bot.message_handler(commands=['rooms'])
-# def get_weather(message):
-#     bot.send_message(message.chat.id, "hello")
-#
-#
-# bot.polling(none_stop=True)
+# rooms = DataBaseWork.get_all_rooms_user(user_id)
+# print(rooms)
+# print(rooms[0].title)
+
+@bot.message_handler(commands=['start'])
+def get_user(message):
+    user_id = message.chat.id
+    user = DataBaseWork.get_user(user_id)
+    if user is None:
+        user = DataBaseWork.create_user(user_id, message.chat.first_name)
+    bot.send_message(message.chat.id, text='Hello, {name}'.format(name=user.name))
+
+
+@bot.message_handler(commands=['rooms'])
+def get_rooms(message):
+    user_id = message.chat.id
+    rooms = DataBaseWork.get_all_rooms_user(user_id)
+    rooms = [room.title for room in rooms]
+    text = "rooms: " + ', '.join(rooms)
+    bot.send_message(message.chat.id, text=text)
+
+
+@bot.message_handler(commands=['create_room'])
+def create_room(message):
+    user_id = message.chat.id
+    user = DataBaseWork.get_user(user_id)
+    if user is None:
+        get_user(message)
+        user = DataBaseWork.get_user(user_id)
+    room = DataBaseWork.create_room(user.id, 'test_room')
+    bot.send_message(message.chat.id, text=room.title)
+
+bot.polling(none_stop=True)
--- db_connect1.py
+++ db_connect1.py
@@ -24,7 +24,7 @@ class Room(Base):
     __tablename__ = 'rooms'
     id = Column(Integer, primary_key=True)
     title = Column(String(100), nullable=False)
-    owner_id = Column(Integer, ForeignKey('people.id'))
+    owner_id = Column(Integer, ForeignKey('people.id'), nullable=False)
 
 
 class DataBaseWork:
@@ -59,5 +59,25 @@ class DataBaseWork:
     @classmethod
     def get_all_rooms_user(cls, user_id):
         with Session(autoflush=False, bind=engine) as db:
-            rooms = db.query(Room).filter(Room.owner_id == user_id).all()
+            rooms = db.query(
+                Person.id,
+                Person.id_user,
+                Person.name,
+                Room.title
+            ).join(Room).filter(Person.id_user==user_id).all()
         return rooms
+
+    @classmethod
+    def get_room(cls, room_id):
+        with Session(autoflush=False, bind=engine) as db:
+            room = db.query(Room).filter(Room.id==room_id).first()
+        return room
+
+    @classmethod
+    def create_room(cls, user_id, title):
+        with Session(autoflush=False, bind=engine) as db:
+            room = Room(owner_id=user_id, title=title)
+            db.add(room)
+            db.commit()
+            room_id = room.id
+        return cls.get_room(room_id)
